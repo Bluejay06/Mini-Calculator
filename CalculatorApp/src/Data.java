1cb4c7dde5a3c0d592324ecdc734400b5330c065
Bluejay06 <kevin.paliza2@gmail.com>
2021-12-27 21:34:20 -0500

    Added Power Button instead of Square Root

--- CalculatorApp/src/Data.java
+++ CalculatorApp/src/Data.java
@@ -6,20 +6,25 @@ public class Data {
 	public float calculate(long num1, long num2, String operator) {
 		switch(operator) {
 		case "+":
+			//Addition
 			return num1 + num2;
 		case "-":
+			//Subtraction
 			return num1 - num2;
 		case "*":
+			//Multiple
 			return num1 * num2;
 		case "/":
+			//Divide
 			if(num2 == 0) {
 				return 0;
 			}
 			else {
 				return num1/ num2;
 			}
-		case "SR":
-			return (float) Math.sqrt(num1);
+		case "PR":
+			//num1^num2 Power
+			return (float) Math.pow(num1,num2);
 		
 		default:
 			return 0;
